5797e6da236dd6ff95cf5f63de5777d4d5e27896
Elea Yuen <eleayuen@gmail.com>
2026-05-13 04:56:30 +0800

    v 1.6.5

--- server.ts
+++ server.ts
@@ -30,7 +30,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 // Middleware to parse JSON bodies
 app.use(express.json({ limit: '50mb' }));
